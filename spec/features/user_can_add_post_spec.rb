d85587dcc06cff5cf89520b4b42663b052e088d6
Josh <joshlim092@gmail.com>
2022-02-12 17:28:22 +0000

    add spec for creating new posts"

--- spec/features/user_can_add_post_spec.rb
+++ spec/features/user_can_add_post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "timeline", type: :feature do
   scenario "User can add posts" do 
     post = Post.create(message: "Hello, world!")
-    visit "posts/index"
-    expect(post).to have_content("Hello, world!")
+    visit "posts"
+    expect(page).to have_content("Hello, world!")
   end 
 end 
